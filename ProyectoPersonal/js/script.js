46c09bb135cf87a74ac0029b427b7644b92f801f
MajinBlas <braisvial96@hotmail.com>
2023-05-26 14:47:12 +0200

    5.26.1

---
+++ ProyectoPersonal/js/script.js
@@ -0,0 +1,9 @@
+function mostrarPopUp() {
+    var popUp = document.getElementById("miPopUp");
+    popUp.style.display = "block";
+  }
+  
+  function cerrarPopUp() {
+    var popUp = document.getElementById("miPopUp");
+    popUp.style.display = "none";
+  }
